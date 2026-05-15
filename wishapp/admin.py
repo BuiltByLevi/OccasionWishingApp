e0f112f8fd1865130bea38ad2ed618ca5dd9a44d
ptktht0296 <116147123+ptktht0296@users.noreply.github.com>
2026-05-16 02:45:26 +0530

    Fix import error in apps.py

--- wishapp/admin.py
+++ wishapp/admin.py
@@ -4,12 +4,7 @@ from .models import OccasionMessage, FriendWish
 @admin.register(OccasionMessage)
 class OccasionMessageAdmin(admin.ModelAdmin):
     list_display = ['occasion', 'title', 'theme_color', 'emojis']
-    list_editable = ['title', 'theme_color', 'emojis']
-    search_fields = ['occasion', 'title']
-    list_filter = ['occasion']
 
 @admin.register(FriendWish)
 class FriendWishAdmin(admin.ModelAdmin):
     list_display = ['name', 'message', 'added_date']
-    search_fields = ['name']
-    list_filter = ['added_date']
