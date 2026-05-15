0c743dac0b08841851c7a99124f8ae714be72e5b
ptktht0296 <116147123+ptktht0296@users.noreply.github.com>
2026-05-16 03:17:47 +0530

    Fix urls.py: remove references to missing friends_wall view

--- wishapp/urls.py
+++ wishapp/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('wall/', views.friends_wall, name='friends_wall'),
-    path('add-friend/', views.add_friend, name='add_friend'),
-]
+    ]
